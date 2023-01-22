39b3c7242d9ddafad4cb0623f2e52c557deabfef
thakyZ <thakyz@outlook.com>
2023-01-22 14:41:52 -0800

    Improved Rename Files in Folder cmdlet

--- Rename-FilesInFolder.ps1
+++ Rename-FilesInFolder.ps1
@@ -1,91 +1,111 @@
-function Rename-FilesInFolder {
-  [CmdletBinding(DefaultParameterSetName = "Path")]
-  param(
-    # Path of folder to rename files of
-    [Parameter(Mandatory = $true, Position = 0, ParameterSetName = "Path", HelpMessage = "Enter one or more filenames")]
-    [Parameter(Mandatory = $true, Position = 0, ParameterSetName = "PathAll")]
-    [string[]]
-    $Path,
-    # Path of folder to rename files of
-    [Parameter(Mandatory = $true, Position = 0, ParameterSetName = "LiteralPathAll")]
-    [Parameter(Mandatory = $true, Position = 0, ParameterSetName = "LiteralPath", HelpMessage = "Enter a single filename", ValueFromPipeline = $true)]
-    [string]
-    $LiteralPath,
-    # Enable Extension renaming
-    [Parameter(Mandatory = $false, Position = 1, HelpMessage = "Enable Extension renaming", ParameterSetName = "Extension")]
-    [Parameter(Mandatory = $false, Position = 1, HelpMessage = "Enable Extension renaming", ParameterSetName = "Path")]
-    [Parameter(Mandatory = $false, Position = 1, HelpMessage = "Enable Extension renaming", ParameterSetName = "LiteralPath")]
-    [switch]
-    $Extention,
-    # Enable Name renaming
-    [Parameter(Mandatory = $false, Position = 1, HelpMessage = "Enable Name renaming", ParameterSetName = "Name")]
-    [Parameter(Mandatory = $false, Position = 1, HelpMessage = "Enable Name renaming", ParameterSetName = "Path")]
-    [Parameter(Mandatory = $false, Position = 1, HelpMessage = "Enable Name renaming", ParameterSetName = "LiteralPath")]
-    [switch]
-    $Name,
-    # Enable Name and Extension renaming
-    [Parameter(Mandatory = $false, Position = 1, HelpMessage = "Enable Name and Extension renaming", ParameterSetName = "NameAndExtension")]
-    [Parameter(Mandatory = $false, Position = 1, HelpMessage = "Enable Name and Extension renaming", ParameterSetName = "Path")]
-    [Parameter(Mandatory = $false, Position = 1, HelpMessage = "Enable Name and Extension renaming", ParameterSetName = "LiteralPath")]
-    [switch]
-    $NameExtension,
-    # The string to replace, the string to replace with
-    [Parameter(Mandatory = $false, Position = 2, HelpMessage = "The string to replace, the string to replace with", ParameterSetName = "Extension")]
-    [Parameter(Mandatory = $false, Position = 2, HelpMessage = "The string to replace, the string to replace with", ParameterSetName = "Name")]
-    [Parameter(Mandatory = $false, Position = 2, HelpMessage = "The string to replace, the string to replace with", ParameterSetName = "NameAndExtension")]
-    [Parameter(Mandatory = $false, Position = 2, HelpMessage = "The string to replace, the string to replace with", ParameterSetName = "Path")]
-    [Parameter(Mandatory = $false, Position = 2, HelpMessage = "The string to replace, the string to replace with", ParameterSetName = "LiteralPath")]
-    [string[]]
-    $Replace,
-    # Recurse the path provided
-    [Parameter(Mandatory = $false, Position = 3, HelpMessage = "Recurse the path provided", ParameterSetName = "Path")]
-    [Parameter(Mandatory = $false, Position = 3, HelpMessage = "Recurse the path provided", ParameterSetName = "PathAll")]
-    [switch]
-    $Recurse
-  )
+#function Rename-FilesInFolder {
+[CmdletBinding(DefaultParameterSetName = "Path")]
+param(
+  # Path of folder to rename files of
+  [Parameter(Mandatory = $true, Position = 0, ParameterSetName = "Path", HelpMessage = "Enter one or more filenames")]
+  [Parameter(Mandatory = $true, Position = 0, ParameterSetName = "PathAll")]
+  [string[]]
+  $Path,
+  # Path of folder to rename files of
+  [Parameter(Mandatory = $true, Position = 0, ParameterSetName = "LiteralPathAll")]
+  [Parameter(Mandatory = $true, Position = 0, ParameterSetName = "LiteralPath", HelpMessage = "Enter a single filename", ValueFromPipeline = $true)]
+  [string]
+  $LiteralPath,
+  # Enable Extension renaming
+  [Parameter(Mandatory = $false, HelpMessage = "Enable Extension renaming", ParameterSetName = "Extension")]
+  [Parameter(Mandatory = $false, HelpMessage = "Enable Extension renaming", ParameterSetName = "Path")]
+  [Parameter(Mandatory = $false, HelpMessage = "Enable Extension renaming", ParameterSetName = "LiteralPath")]
+  [switch]
+  $Extension,
+  # Enable Name renaming
+  [Parameter(Mandatory = $false, HelpMessage = "Enable Name renaming", ParameterSetName = "Name")]
+  [Parameter(Mandatory = $false, HelpMessage = "Enable Name renaming", ParameterSetName = "Path")]
+  [Parameter(Mandatory = $false, HelpMessage = "Enable Name renaming", ParameterSetName = "LiteralPath")]
+  [switch]
+  $Name,
+  # Enable Name and Extension renaming
+  [Parameter(Mandatory = $false, HelpMessage = "Enable Name and Extension renaming", ParameterSetName = "NameAndExtension")]
+  [Parameter(Mandatory = $false, HelpMessage = "Enable Name and Extension renaming", ParameterSetName = "Path")]
+  [Parameter(Mandatory = $false, HelpMessage = "Enable Name and Extension renaming", ParameterSetName = "LiteralPath")]
+  [switch]
+  $NameExtension,
+  # The string to replace, the string to replace with
+  [Parameter(Mandatory = $false, HelpMessage = "The string to replace, the string to replace with", ParameterSetName = "Extension")]
+  [Parameter(Mandatory = $false, HelpMessage = "The string to replace, the string to replace with", ParameterSetName = "Name")]
+  [Parameter(Mandatory = $false, HelpMessage = "The string to replace, the string to replace with", ParameterSetName = "NameAndExtension")]
+  [Parameter(Mandatory = $false, HelpMessage = "The string to replace, the string to replace with", ParameterSetName = "Path")]
+  [Parameter(Mandatory = $false, HelpMessage = "The string to replace, the string to replace with", ParameterSetName = "LiteralPath")]
+  [string[]]
+  $Replace,
+  # Recurse the path provided
+  [Parameter(Mandatory = $false, HelpMessage = "Recurse the path provided", ParameterSetName = "Path")]
+  [Parameter(Mandatory = $false, HelpMessage = "Recurse the path provided", ParameterSetName = "PathAll")]
+  [switch]
+  $Recurse
+)
 
-  Begin {
-    If ($Name) {
-      If ($Path) {
-        $Files = Get-ChildItem -Path $Path -Recurse:$Recurse -Filter "$($Replace[0]).*" -File;
-      }
-      Else {
-        $Files = Get-ChildItem -LiteralPath $LiteralPath -Filter -Filter "$($Replace[0]).*" -File;
-      }
+Begin {
+  If ($Name) {
+    If ($Path) {
+      $Files = Get-ChildItem -Path $Path -Recurse:$Recurse -Filter "$($Replace[0]).*" -File;
     }
-    Elseif ($Extention) {
-      If ($Path) {
-        $Files = Get-ChildItem -Path $Path -Recurse:$Recurse -Filter "*.$($Replace[0])" -File;
-      }
-      Else {
-        $Files = Get-ChildItem -LiteralPath $LiteralPath -Filter "*.$($Replace[0])" -File;
-      }
-    }
-    Elseif ($NameExtension) {
-      If ($Path) {
-        $Files = Get-ChildItem -Path $Path -Recurse:$Recurse -Filter "$($Replace[0])" -File;
-      }
-      Else {
-        $Files = Get-ChildItem -LiteralPath $LiteralPath -Filter "$($Replace[0])" -File;
-      }
-    }
-    If ($Replace.Lengh -ne 2) {
-      Write-Error -Message "Replace variable should be the string you want to replace comma sepreated by the string you want to replace it with."
+    Else {
+      $Files = Get-ChildItem -LiteralPath $LiteralPath -Filter -Filter "$($Replace[0]).*" -File;
     }
   }
-  Process {
-    ForEach ($File in $Files) {
-      If ($Name) {
-        Rename-Item -LiteralPath $File.FullName -NewName "$($Replace[1]).$($File.Extension)"
+  Elseif ($Extension) {
+    If ($Path) {
+      $Files = Get-ChildItem -Path $Path -Recurse:$Recurse -Filter "*.$($Replace[0])" -File;
+    }
+    Else {
+      $Files = Get-ChildItem -LiteralPath $LiteralPath -Filter "*.$($Replace[0])" -File;
+    }
+  }
+  Elseif ($NameExtension) {
+    If ($Path) {
+      $Files = Get-ChildItem -Path $Path -Recurse:$Recurse -Filter "$($Replace[0])" -File;
+    }
+    Else {
+      $Files = Get-ChildItem -LiteralPath $LiteralPath -Filter "$($Replace[0])" -File;
+    }
+  }
+  If ($Replace.Length -ne 2) {
+    Write-Error -Message "Replace variable should be the string you want to replace comma sepreated by the string you want to replace it with."
+  }
+}
+Process {
+  Function Test-PathExists() {
+    Param(
+      [string]
+      $Directory,
+      [String]
+      $Path
+    )
+    $Val = $Path
+    $RegEx = "(\s\((\d\)))"
+    If (Test-Path -Path $Path) {
+      $NewNum = 1;
+      If ($Path -match $RegEx) {
+        $NewNum = parseInt(($Path -replace $RegEx, "$2")) + 1;
       }
-      Elseif ($Extention) {
-        Rename-Item -LiteralPath $File.FullName -NewName "$($File.BaseName).$($Replace[1])"
-      }
-      Elseif ($NameExtension) {
-        Rename-Item -LiteralPath $File.FullName -NewName "$($Replace[1])"
-      }
+      $Val = Test-PathExists -Path "$($Directory)\$($Val.Split(".")[0] -replace $RegEx, '') ($($NewNum)).$($Val.Split(".")[1])"
+    }
+    Return $Val
+  }
+
+  ForEach ($File in $Files) {
+    If ($Name) {
+      Writ-Host (Test-PathExists -Path "$($File.Directory.FullName)\$($Replace[1]).$($File.Extension)")
+      Rename-Item -LiteralPath $File.FullName -NewName (Test-PathExists -Directory "$($File.Directory.FullName)" -Path "$($Replace[1]).$($File.Extension)")
+    }
+    Elseif ($Extension) {
+      Rename-Item -LiteralPath $File.FullName -NewName (Test-PathExists -Directory "$($File.Directory.FullName)" -Path "$($File.BaseName).$($Replace[1])")
+    }
+    Elseif ($NameExtension) {
+      Rename-Item -LiteralPath $File.FullName -NewName (Test-PathExists -Directory "$($File.Directory.FullName)" -Path "$($Replace[1])")
     }
   }
 }
+#}
 
-Export-ModuleMember -Function Rename-FilesInFolder
+#Export-ModuleMember -Function Rename-FilesInFolder

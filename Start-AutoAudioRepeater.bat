d048df964dcd073a4deff6ae005cb23e71fcda12
thakyZ <thakyz@outlook.com>
2023-01-28 17:03:46 -0800

    Added auto launch audio repeater

---
+++ Start-AutoAudioRepeater.bat
@@ -0,0 +1,2 @@
+@echo off
+start /min "" "D:\Progra~1\Virtual Audio Cable\audiorepeater.exe" /SamplingRate:48000 /BitsPerSample:16 /Input:"Line 1 (Virtual Audio Cable)" /Output:"Headphones (High Definition Aud" /ChanCfg:"Stereo" /AutoStart

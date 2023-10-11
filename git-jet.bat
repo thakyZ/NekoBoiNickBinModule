d7e71169c7541ef3ec4d6f77f1d45d8854ce2b4f
thakyZ <950594+thakyZ@users.noreply.github.com>
2023-10-11 09:53:10 -0700

    Updated gitjet

--- git-jet.bat
+++ git-jet.bat
@@ -1,9 +1,9 @@
 @echo off
-set SSHAgent="C:\Windows\System32\OpenSSH\ssh-agent.exe"
-set SSH="C:\Windows\System32\OpenSSH\ssh.exe"
-set GIT="D:\Files\System\Programs\Git\bin\git.exe"
-set PATH="D:\Files\System\Programs\Git\bin;C:\Windows\System32\OpenSSH\;%PATH%"
-set KEY="C:\Users\thaky\.ssh\id_rsa"
+set SSHAgent="%WINDIR%\System32\OpenSSH\ssh-agent.exe"
+set SSH="%WINDIR%\System32\OpenSSH\ssh.exe"
+set GIT="%APROG_DIR%\Git\bin\git.exe"
+set PATH="%APROG_DIR%\Git\bin;%WINDIR%\System32\OpenSSH\;%PATH%"
+set KEY="%USERPROFILE%\.ssh\id_rsa"
 
 set GIT_SSH_COMMAND="%SSH% -i %KEY%"
 %GIT% %*

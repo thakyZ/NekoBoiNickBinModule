c9deece688ef9b3ce2844870c93ee3ee01c90d0e
thakyZ <950594+thakyZ@users.noreply.github.com>
2023-10-12 05:35:53 -0700

    Updated Start AutoAudioRepeater

--- Start-AutoAudioRepeater.ps1
+++ Start-AutoAudioRepeater.ps1
@@ -5,51 +5,51 @@ Param(
 	$Set
 )
 
-Function Cut-String([int]$Length, [string]$Value) {
-  If ($Value.Length -gt $Length) {
+Function Invoke-CutString([int]$Length, [string]$Value) {
+	If ($Value.Length -gt $Length) {
 		Return $Value.SubString(0, $Length);
 	}
 	Return $Value;
 }
 
 class Config {
-	[string] $Input         = "High Definition Audio";
-  [string] $Output        = "High Definition Audio";
-  [int]    $SamplingRate  = 32000;
-  [int]    $BitsPerSample = 16;
-  [int]    $Channels      = 2;
-  [string] $ChannelCfg    = "Stereo";
-  [int]    $BufferMs      = 100;
-  [int]    $BufferParts   = 12;
-  [int]    $Prefill       = 70;
-  [int]    $ResyncAt      = 20;
-  [string] $Priority      = "Normal";
+	[string] $_Input = "High Definition Audio";
+	[string] $Output = "High Definition Audio";
+	[int]    $SamplingRate = 32000;
+	[int]    $BitsPerSample = 16;
+	[int]    $Channels = 2;
+	[string] $ChannelCfg = "Stereo";
+	[int]    $BufferMs = 100;
+	[int]    $BufferParts = 12;
+	[int]    $Prefill = 70;
+	[int]    $ResyncAt = 20;
+	[string] $Priority = "Normal";
 
 	Config([psCustomObject]$Config) {
-	  ForEach ($Item in $Config.PSObject.Properties) {
+		ForEach ($Item in $Config.PSObject.Properties) {
 			$Split = ($Item -Split "=");
 			$Name = ($Split[0] -Split " ");
 			$Value = $Split[1];
 
 			Switch ($Name.ToLower()) {
-				"input"         { If ($Null -ne $Value -and $Value -ne "null") { $this.Input         = (Cut-String -Length 31 -Value $Value) } }
-				"output"        { If ($Null -ne $Value -and $Value -ne "null") { $this.Output        = (Cut-String -Length 31 -Value $Value) } }
-				"samplingrate"  { If ($Null -ne $Value -and $Value -ne "null") { $this.SamplingRate  = $Value } }
+				"input" { If ($Null -ne $Value -and $Value -ne "null") { $this._Input = (Invoke-CutString -Length 31 -Value $Value) } }
+				"output" { If ($Null -ne $Value -and $Value -ne "null") { $this.Output = (Invoke-CutString -Length 31 -Value $Value) } }
+				"samplingrate" { If ($Null -ne $Value -and $Value -ne "null") { $this.SamplingRate = $Value } }
 				"bitspersample" { If ($Null -ne $Value -and $Value -ne "null") { $this.BitsPerSample = $Value } }
-				"channels"      { If ($Null -ne $Value -and $Value -ne "null") { $this.Channels      = $Value } }
-				"channelcfg"    { If ($Null -ne $Value -and $Value -ne "null") { $this.ChannelCfg    = (Cut-String -Length 31 -Value $Value) } }
-				"bufferms"      { If ($Null -ne $Value -and $Value -ne "null") { $this.BufferMs      = $Value } }
-				"bufferparts"   { If ($Null -ne $Value -and $Value -ne "null") { $this.BufferParts   = $Value } }
-				"prefill"       { If ($Null -ne $Value -and $Value -ne "null") { $this.Prefill       = $Value } }
-				"resyncat"      { If ($Null -ne $Value -and $Value -ne "null") { $this.ResyncAt      = $Value } }
-				"priority"      { If ($Null -ne $Value -and $Value -ne "null") { $this.Priority      = (Cut-String -Length 31 -Value $Value) } }
+				"channels" { If ($Null -ne $Value -and $Value -ne "null") { $this.Channels = $Value } }
+				"channelcfg" { If ($Null -ne $Value -and $Value -ne "null") { $this.ChannelCfg = (Invoke-CutString -Length 31 -Value $Value) } }
+				"bufferms" { If ($Null -ne $Value -and $Value -ne "null") { $this.BufferMs = $Value } }
+				"bufferparts" { If ($Null -ne $Value -and $Value -ne "null") { $this.BufferParts = $Value } }
+				"prefill" { If ($Null -ne $Value -and $Value -ne "null") { $this.Prefill = $Value } }
+				"resyncat" { If ($Null -ne $Value -and $Value -ne "null") { $this.ResyncAt = $Value } }
+				"priority" { If ($Null -ne $Value -and $Value -ne "null") { $this.Priority = (Invoke-CutString -Length 31 -Value $Value) } }
 			}
 		}
 	}
 
-  [string[]]ToArray() {
+	[string[]]ToArray() {
 		Return @(
-		  "/Input:`"$($this.Input)`"",
+			"/Input:`"$($this._Input)`"",
 			"/Output:`"$($this.Output)`"",
 			"/SamplingRate:$($this.SamplingRate)",
 			"/BitsPerSample:$($this.BitsPerSample)",
@@ -61,7 +61,7 @@ class Config {
 			"/Prefill:$($this.Prefill)",
 			"/ResyncAt:$($this.ResyncAt)",
 			"/Priority:`"$($this.Priority)`""
-	  );
+		);
 	}
 }
 
@@ -69,26 +69,26 @@ $Json = $Null;
 [Config]$Config = $Null;
 
 Try {
-	$Json = (Get-Content -LiteralPath (Join-Path -Path $PSScriptRoot -ChildPath "Settings.json") | ConvertFrom-Json);
+	$Json = (Get-Content -LiteralPath (Join-Path -Path $PSScriptRoot -ChildPath "config.json") | ConvertFrom-Json);
 
-  If ($Set -ne "" -and $Null -ne $Set) {
+	If (-not [string]::IsNullOrEmpty($Set)) {
 		$Fetched = ($Json.AudioRepeaterSettings.Presets | Where-Object { $_.Name.ToLower() -eq $Set.ToLower() }).Settings
 
-    If ($Null -eq $Fetched) {
+		If ($Null -eq $Fetched) {
 			Throw "Specified preset `"$($Set)`" does not exist in the config.";
 		} Else {
-		  $Json.AudioRepeaterSettings.Current = $Fetched;
+			$Json.AudioRepeaterSettings.Current = $Fetched;
 		}
 	}
 
 	$Config = $Json.AudioRepeaterSettings.Current;
 } Catch {
 	Write-Error -Exception $_.Exception
-	Write-Warning -Message "Failed to read Audio Repeater Settings from `"$((Join-Path -Path $PSScriptRoot -ChildPath "Settings.json"))`". Trying `"$((Join-Path -Path $PSScriptRoot -ChildPath "AutoAudioRepeater.cfg"))`" instead.";
+	Write-Warning -Message "Failed to read Audio Repeater Settings from `"$((Join-Path -Path $PSScriptRoot -ChildPath "config.json"))`". Trying `"$((Join-Path -Path $PSScriptRoot -ChildPath "AutoAudioRepeater.cfg"))`" instead.";
 	Try {
-    $ConfigFile = $Null;
+		$ConfigFile = $Null;
 
-    If ($Null -eq $env:APROG_DIR) {
+		If ($Null -eq $env:APROG_DIR) {
 			$ConfigFile = (Join-Path -Path $PSScriptRoot -ChildPath "AutoAudioRepeater.cfg");
 		} Else {
 			$ConfigFile = (Join-Path -Path $env:APROG_DIR -ChildPath "bin" -AdditionalChildPath "AutoAudioRepeater.cfg");
@@ -97,32 +97,32 @@ Try {
 		$_Config = @{}
 
 		ForEach ($Line in (Get-Content -LiteralPath $ConfigFile)) {
-		  $_Line = (($Line -Replace "/", "") -Replace "`"", "");
-		  $Item = ($_Line -Split (":"))
+			$_Line = (($Line -Replace "/", "") -Replace "`"", "");
+			$Item = ($_Line -Split (":"))
 			$_Config[$Item[0]] = $Item[1];
 		}
 
 		$Config = New-Object -ItemType Config -ArgumentList @($_Config);
 	} Catch {
-	  Write-Error -Exception $_.Exception
-	  Throw "Failed to read all config types."
+		Write-Error -Exception $_.Exception
+		Throw "Failed to read all config types."
 	}
 }
 
 $VirtualAudioCable = "audiorepeater.exe";
-$VAC_BackupPath = (Join-Path -Path $env:ProgramFiles -ChildPath "Virtual Audio Cable" -AdditionalChildPath $VirtualAudioCable)
+$VAC_BackupPath = (Join-Path -Path $env:ProgramFiles -ChildPath "Virtual Audio Cable" -AdditionalChildPath $VirtualAudioCable);
 
 If ($Null -eq (Get-Command -Name $VirtualAudioCable -ErrorAction SilentlyContinue)) {
   If (Test-Path -Path $VAC_BackupPath) {
-	$VirtualAudioCable = $VAC_BackupPath;
+	  $VirtualAudioCable = $VAC_BackupPath;
   } Else {
     Write-Error -Message "Virtual Audio Cable not found on path or default directory $($VAC_BackupPath)"
-	Exit 1
+	  Exit 1;
   }
 }
 
 If ($Null -ne (Get-Process -Name "audiorepeater" -ErrorAction SilentlyContinue)) {
-  Exit 0
+  Exit 0;
 }
 
 $Arguments = $Config.ToArray();

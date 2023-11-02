d85b4e388b5a0b621779510b1bdb29b21b5fcaa0
thakyZ <950594+thakyZ@users.noreply.github.com>
2023-11-02 08:16:24 -0700

    add: script to proxy rebuild-binfolder

---
+++ Rebuild-BinFolder.ps1
@@ -0,0 +1,42 @@
+param(
+  # Open the log folder
+  [Parameter(Position = 0, Mandatory = $false, HelpMessage = "Opens the output log file.", ParameterSetName = "OpenOutputFolder")]
+  [Switch]
+  $OpenOutput,
+  # Pass arguments to the binary file
+  [Parameter(Position = 0, Mandatory = $false, HelpMessage = "Displays the help message.")]
+  [Switch]
+  $Help
+)
+
+$CurrentDirectory = $PWD;
+
+Function Get-ProgramDirectory() {
+  If (-not ([string]::IsNullOrEmpty($env:APROG_DIR))) {
+    Return $env:APROG_DIR;
+  }
+  Else {
+    Return (Get-Item -Path $PSScriptRoot).Parent.FullName;
+  }
+}
+
+$APROG_DIR = Get-ProgramDirectory;
+
+$RebuildBinFolder = (Join-Path -Path $APROG_DIR -ChildPath "Rebuild-BinFolder");
+
+$RebuildBinBinary = (Join-Path -Path $RebuildBinFolder -ChildPath "Rebuild-BinFolder.exe");
+
+Set-Location -Path $RebuildBinFolder;
+
+if ($Help) {
+  & "$RebuildBinBinary" "-?"
+}
+else {
+  & "$RebuildBinBinary"
+}
+
+if ($OpenOutput) {
+  Start-Process (Join-Path -Path $RebuildBinFolder -ChildPath "output.log");
+}
+
+Set-Location -Path $CurrentDirectory;

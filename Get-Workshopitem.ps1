1d625e9f7eade7920544f7169e4330258c91ad0c
thakyZ <950594+thakyZ@users.noreply.github.com>
2023-11-02 08:13:13 -0700

    add: script to get workshop item from site

---
+++ Get-Workshopitem.ps1
@@ -0,0 +1,23 @@
+[CmdletBinding()]
+param (
+  [Parameter(HelpMessage="The ID for the workshop item.",Mandatory=$true)]
+  [string]
+  $WorkshopId
+)
+
+if ($null -eq $WorkshopId)
+{
+  Return 1;
+}
+
+Import-Module PowerHTML
+
+$WorkshopItemUri = 'https://steamcommunity.com/sharedfiles/filedetails/?id='+$WorkshopId
+
+$Response = ConvertFrom-Html -Uri $WorkshopItemUri
+
+if ($null -ne ($Response.SelectNodes('//h1') | Where-Object { $_.InnerTest -eq "Sorry!" }))
+{
+  $SteamID_b = (($Response.SelectNodes('//a[class="apphub_sectionTab"]') | Where-Object { $_.SelectNodes('//span') | Where-Object { $_.InnerText -eq "All" }})[0]);
+  $SteamID =  $SteamID_b.GetAttributeValue("href", "").Replace('https://steamcommunity.com/app/','');
+}

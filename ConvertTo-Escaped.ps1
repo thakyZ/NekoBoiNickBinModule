626b12d4006e930d6f1b99f97595d2f06697c0cd
thakyZ <950594+thakyZ@users.noreply.github.com>
2023-11-02 08:08:48 -0700

    add: global script to escape strings for pwsh

---
+++ ConvertTo-Escaped.ps1
@@ -0,0 +1,47 @@
+Param(
+  # The string to escape.
+  [Parameter(Mandatory = $True, Position = 0, ValueFromPipeline = $True, HelpMessage = "The string to escape.")]
+  [string[]]
+  $InputObject
+)
+
+Begin {
+  $CharsToEscape = @(
+    "["
+    "]"
+    "+"
+    "("
+    ")"
+  );
+  $Output = @()
+}
+Process {
+  Function Test-ContainsEscapable() {
+    Param(
+      # The string to escape.
+      [Parameter(Mandatory = $True, Position = 0, ValueFromPipelineByPropertyName = $True, ValueFromRemainingArguments = $True, ValueFromPipeline = $True, HelpMessage = "The string to escape.")]
+      [Alias("InputObject")]
+      [string]
+      $InputObject2
+    )
+
+    ForEach ($Char in $CharsToEscape) {
+      If ($InputObject2 -Match "(?<!``)\$($Char)") {
+        $InputObject2 = $InputObject2 -Replace "\$($Char)", "``$($Char)"
+      }
+    }
+
+    Return $InputObject2;
+  }
+
+  If ($InputObject.Length -le 0 -or $Null -eq $InputObject) {
+    Throw "`$InputObject is empty or null.";
+  }
+
+  ForEach ($Input in $InputObject) {
+    $Output += @(Test-ContainsEscapable -InputObject $Input)
+  }
+}
+End {
+  Return $Output;
+}

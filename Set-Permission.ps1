80ef9d2c5ad1bfd6ecfb8783b67a87b2b3fd53cd
thakyZ <950594+thakyZ@users.noreply.github.com>
2024-02-22 07:01:56 -0800

    add: set permission script (wip) applies acl permissions recursively on a folder among other things.

---
+++ Set-Permission.ps1
@@ -0,0 +1,55 @@
+Param(
+  # Specifies a path to one or more locations. Unlike the Path parameter, the value of the LiteralPath parameter is
+  # used exactly as it is typed. No characters are interpreted as wildcards. If the path includes escape characters,
+  # enclose it in single quotation marks. Single quotation marks tell Windows PowerShell not to interpret any
+  # characters as escape sequences.
+  [Parameter(Mandatory = $True,
+             Position = 0,
+             ParameterSetName = "LiteralPath",
+             ValueFromPipelineByPropertyName = $True,
+             HelpMessage = "Literal path to one or more locations.")]
+  [Alias("PSPath")]
+  [ValidateNotNullOrEmpty()]
+  [string[]]
+  $Path
+)
+
+Function Test-AclContains($Acl) {
+  Return ($Null -ne ($Acl.Access | Where-Object {
+    $Access = $_;
+    Return $Access.IdentityReference -eq "Oscar11\thaky" -and $Access.IsInherited -eq $False;
+  }));
+}
+
+Function Get-AclAccessIndex($Access) {
+  $Index = 0;
+  For ($Index; $Index -lt $Access.Count; $Index++) {
+    If ($Access[$Index].IdentityReference -eq "Oscar11\thaky" -and $Access.IsInherited -eq $False) {
+      Break;
+    }
+  }
+  Return $Index;
+}
+
+$Items = (Get-ChildItem -Path $Path -Recurse | Where-Object {
+  $SubItem = $_.FullName;
+  $Acl = (Get-Acl -Path $SubItem);
+  Return (Test-AclContains -Acl $Acl);
+});
+
+Write-Output "Found Items Total: $($Items.Length)"
+
+$Items | ForEach-Object {
+  Try {
+    $SubItem = $_.FullName;
+    $Acl = (Get-Acl $SubItem);
+    $BeforeCount = $Acl.Access.Count;
+    $AccessIndex = Get-AclAccessIndex -Acl $Acl.Access;
+    $Acl.RemoveAccessRuleSpecific($Acl.Access[$AccessIndex]);
+    $AfterCount = $Acl.Access.Count;
+    Write-Output "File: $($SubItem)`nBefore Count: $($BeforeCount)`nFound Index: $($AccessIndex)`nAfter Count: $($AfterCount)`n";
+    # Set-Acl -Path $SubItem -AclObject $Acl;
+  } Catch {
+    Write-Error -Exception $_.Exception -Message $_.Message;
+  }
+}

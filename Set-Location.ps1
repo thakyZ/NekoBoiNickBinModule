00d3758d74f159686f3bfec8b404af9c8652686f
thakyZ <950594+thakyZ@users.noreply.github.com>
2024-02-22 07:03:05 -0800

    update: set location proxy script

--- Set-Location.ps1
+++ Set-Location.ps1
@@ -1,182 +1,216 @@
-[CmdletBinding(DefaultParameterSetName = 'Path', HelpUri = 'https://go.microsoft.com/fwlink/?LinkID=2097049')]
-param(
-  [Parameter(ParameterSetName = 'Path', Position = 0, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
-  [string]
-  ${Path},
+[CmdletBinding(DefaultParameterSetName = "Path", HelpUri = "https://go.microsoft.com/fwlink/?LinkID=2097049")]
+Param(
+  [Parameter(ParameterSetName = "Path",
+             Position = 0,
+             ValueFromPipeline = $True,
+             ValueFromPipelineByPropertyName = $True)]
+  [System.String]
+  $Path,
 
-  [Parameter(ParameterSetName = 'LiteralPath', Mandatory = $true, ValueFromPipelineByPropertyName = $true)]
-  [Alias('PSPath', 'LP')]
-  [string]
-  ${LiteralPath},
+  [Parameter(ParameterSetName = "LiteralPath",
+             Mandatory = $True,
+             ValueFromPipelineByPropertyName = $True)]
+  [Alias("PSPath", "LP")]
+  [System.String]
+  $LiteralPath,
 
   [switch]
-  ${PassThru},
+  $PassThru,
 
-  [Parameter(ParameterSetName = 'Stack', ValueFromPipelineByPropertyName = $true)]
+  [Parameter(ParameterSetName = "Stack",
+             ValueFromPipelineByPropertyName = $True)]
   [string]
-  ${StackName})
+  $StackName
+)
 
+Function Invoke-ForFive {
+  Write-Host "Soup0"
+  If ($PSCmdlet.ParameterSetName -eq "LiteralPath") {
+    Write-Host "Soup1"
+    Set-Location -LiteralPath $LiteralPath;
+    Write-Host "Soup2"
+    Exit $LastExitCode;
+  }
+  ElseIf ($PSCmdlet.ParameterSetName -eq "Path") {
+    Write-Host "Soup1"
+    Set-Location -Path $Path;
+    Write-Host "Soup2"
+    Exit $LastExitCode;
+  }
+  Write-Host "Soup3"
+}
 
-dynamicparam {
-  try {
-    $targetCmd = $ExecutionContext.InvokeCommand.GetCommand('Microsoft.PowerShell.Management\Set-Location', [System.Management.Automation.CommandTypes]::Cmdlet, $PSBoundParameters)
-    $dynamicParams = @($targetCmd.Parameters.GetEnumerator() | Microsoft.PowerShell.Core\Where-Object { $_.Value.IsDynamic })
-    if ($dynamicParams.Length -gt 0) {
-      $paramDictionary = [Management.Automation.RuntimeDefinedParameterDictionary]::new()
-      foreach ($param in $dynamicParams) {
-        $param = $param.Value
-        Write-Host $param;
-        Write-Host $param.Value;
+Function Invoke-ForSeven {
+  Param()
+  DynamicParam {
+    Try {
+      $TargetCmd = $ExecutionContext.InvokeCommand.GetCommand("Microsoft.PowerShell.Management\Set-Location", [System.Management.Automation.CommandTypes]::Cmdlet, $PSBoundParameters)
+      $DynamicParams = @($TargetCmd.Parameters.GetEnumerator() | Microsoft.PowerShell.Core\Where-Object { $_.Value.IsDynamic })
+      if ($DynamicParams.Length -gt 0) {
+        $ParamDictionary = [Management.Automation.RuntimeDefinedParameterDictionary]::new()
+        foreach ($Param in $DynamicParams) {
+          $Param = $Param.Value
+          Write-Host $Param;
+          Write-Host $Param.Value;
 
-        if (-not $MyInvocation.MyCommand.Parameters.ContainsKey($param.Name)) {
-          $dynParam = [Management.Automation.RuntimeDefinedParameter]::new($param.Name, $param.ParameterType, $param.Attributes)
-          $paramDictionary.Add($param.Name, $dynParam)
+          If (-not $MyInvocation.MyCommand.Parameters.ContainsKey($Param.Name)) {
+            $DynParam = [Management.Automation.RuntimeDefinedParameter]::new($Param.Name, $Param.ParameterType, $Param.Attributes)
+            $paramDictionary.Add($Param.Name, $DynParam)
+          }
+        }
+
+        Return $paramDictionary
+      }
+    }
+    Catch {
+      Throw
+    }
+  }
+
+  Begin {
+    If ($Null -eq $env:PathsToCheckFor) {
+      [System.Environment]::SetEnvironmentVariable("PathsToCheckFor", "")
+    }
+    $env:PathsToCheckFor = [System.Environment]::GetEnvironmentVariable("PathsToCheckFor")
+
+    Function Add-ToPath {
+      Param(
+        # Specifies a path to one or more locations. Unlike the Path parameter, the value of the LiteralPath parameter is
+        # used exactly as it is typed. No characters are interpreted as wildcards. If the path includes escape characters,
+        # enclose it in single quotation marks. Single quotation marks tell Windows PowerShell not to interpret any
+        # characters as escape sequences.
+        [Parameter(Mandatory = $True,
+                   Position = 0,
+                   ParameterSetName = "LiteralPath",
+                   ValueFromPipelineByPropertyName = $True,
+                   HelpMessage = "Literal path to one or more locations.")]
+        [Alias("PSPath", "LP")]
+        [ValidateNotNullOrEmpty()]
+        [System.String[]]
+        $LiteralPath
+      )
+
+      ForEach ($Element in $LiteralPath) {
+        $env:PATH = "$($env:PATH);$($Element)";
+        $env:PathsToCheckFor = [System.Environment]::GetEnvironmentVariable("PathsToCheckFor");
+        [System.Environment]::SetEnvironmentVariable("PathsToCheckFor", "$($env:PathsToCheckFor);$($Element)")
+        if ($env:PathsToCheckFor -match "^;") {
+          [System.Environment]::SetEnvironmentVariable("PathsToCheckFor", ($env:PathsToCheckFor -replace "^;", ""))
+        }
+        Write-Debug -Message "Added $($Element) to Path";
+      }
+      Write-Debug $env:PathsToCheckFor;
+      Write-Debug $env:PathsToCheckFor.Split(";").Length;
+    }
+    Function Clear-AddedPaths {
+      Param()
+      [System.Collections.ArrayList]$PathArray = @();
+      foreach ($element in $env:PathsToCheckFor.Split(";")) {
+        if ("$($env:PATH)".Split(";") -contains $element) {
+          $PathsToCheckFor = [System.Environment]::GetEnvironmentVariable("PathsToCheckFor");
+          Write-Debug $env:PathsToCheckFor;
+          Write-Debug $PathsToCheckFor;
+          $PathsToCheckFor = [System.Collections.ArrayList]("$($env:PathsToCheckFor)".Split(";"));
+          Write-Debug $PathsToCheckFor[0];
+          Write-Debug $env:PathsToCheckFor;
+          $PathArray = [System.Collections.ArrayList]("$($env:PATH)".Split(";"));
+          Write-Debug $env:PathsToCheckFor;
+          $PathArray.Remove($element);
+          Write-Debug $env:PathsToCheckFor;
+          $PathsToCheckFor.Remove($element);
+          Write-Debug $env:PathsToCheckFor;
+          Write-Debug $PathsToCheckFor[0];
+          [System.Environment]::SetEnvironmentVariable("PathsToCheckFor", [String]::Join(";", $PathsToCheckFor));
+          Write-Debug $env:PathsToCheckFor;
+          Write-Debug -Message "Removed $($element) from Path";
         }
       }
-
-      return $paramDictionary
+      #Write-Debug $PathArray;
+      Write-Debug $env:PathsToCheckFor;
+      Write-Debug $env:PathsToCheckFor.Split(";").Length;
+      $env:PATH = [String]::Join(";", $PathArray);
     }
-  }
-  catch {
-    throw
-  }
-}
 
-begin {
-  if ($null -eq $env:PathsToCheckFor) {
-    [System.Environment]::SetEnvironmentVariable('PathsToCheckFor', '')
-  }
-  $env:PathsToCheckFor = [System.Environment]::GetEnvironmentVariable('PathsToCheckFor')
+    Function Resolve-DictionaryPath {
+      Param(
+        # Specifies a path to one or more locations. Unlike the Path parameter, the value of the LiteralPath parameter is
+        # used exactly as it is typed. No characters are interpreted as wildcards. If the path includes escape characters,
+        # enclose it in single quotation marks. Single quotation marks tell Windows PowerShell not to interpret any
+        # characters as escape sequences.
+        [Parameter(ParameterSetName = "LiteralPath",
+                   Mandatory = $True,
+                   ValueFromPipelineByPropertyName = $True,
+                   Position = 0,
+                   HelpMessage = "Literal path to one or more locations.")]
+        [Alias("PSPath", "LP")]
+        [ValidateNotNullOrEmpty()]
+        [System.String]
+        $LiteralPath
+      )
 
-  Function Add-ToPath {
-    param(
-      # Specifies a path to one or more locations. Unlike the Path parameter, the value of the LiteralPath parameter is
-      # used exactly as it is typed. No characters are interpreted as wildcards. If the path includes escape characters,
-      # enclose it in single quotation marks. Single quotation marks tell Windows PowerShell not to interpret any
-      # characters as escape sequences.
-      [Parameter(Mandatory = $true,
-        Position = 0,
-        ParameterSetName = "LiteralPath",
-        ValueFromPipelineByPropertyName = $true,
-        HelpMessage = "Literal path to one or more locations.")]
-      [Alias("PSPath", "LP")]
-      [ValidateNotNullOrEmpty()]
-      [string[]]
-      $LiteralPath
-    )
-
-    foreach ($element in $LiteralPath) {
-      $env:PATH = "$($env:PATH);$($element)";
-      $env:PathsToCheckFor = [System.Environment]::GetEnvironmentVariable('PathsToCheckFor');
-      [System.Environment]::SetEnvironmentVariable('PathsToCheckFor', "$($env:PathsToCheckFor);$($element)")
-      if ($env:PathsToCheckFor -match '^;') {
-        [System.Environment]::SetEnvironmentVariable('PathsToCheckFor', ($env:PathsToCheckFor -replace '^;', ''))
+      $PathDirectories = $LiteralPath.Split("\");
+      If ($PathDirectories -contains "TexTools" -and $PathDirectories -contains "ModPacks") {
+        Add-ToPath -LiteralPath @("$($env:FFXIV)\TexTools");
       }
-      Write-Debug -Message "Added $($element) to Path";
-    }
-    Write-Debug $env:PathsToCheckFor;
-    Write-Debug $env:PathsToCheckFor.Split(';').Length;
-  }
-  Function Clear-AddedPaths {
-    param()
-    [System.Collections.ArrayList]$PathArray = @();
-    foreach ($element in $env:PathsToCheckFor.Split(';')) {
-      if ("$($env:PATH)".Split(';') -contains $element) {
-        $PathsToCheckFor = [System.Environment]::GetEnvironmentVariable('PathsToCheckFor');
-        Write-Debug $env:PathsToCheckFor;
-        Write-Debug $PathsToCheckFor;
-        $PathsToCheckFor = [System.Collections.ArrayList]("$($env:PathsToCheckFor)".Split(';'));
-        Write-Debug $PathsToCheckFor[0];
-        Write-Debug $env:PathsToCheckFor;
-        $PathArray = [System.Collections.ArrayList]("$($env:PATH)".Split(';'));
-        Write-Debug $env:PathsToCheckFor;
-        $PathArray.Remove($element);
-        Write-Debug $env:PathsToCheckFor;
-        $PathsToCheckFor.Remove($element);
-        Write-Debug $env:PathsToCheckFor;
-        Write-Debug $PathsToCheckFor[0];
-        [System.Environment]::SetEnvironmentVariable('PathsToCheckFor', [String]::Join(';', $PathsToCheckFor));
-        Write-Debug $env:PathsToCheckFor;
-        Write-Debug -Message "Removed $($element) from Path";
+      Else {
+        Clear-AddedPaths;
       }
     }
-    #Write-Debug $PathArray;
-    Write-Debug $env:PathsToCheckFor;
-    Write-Debug $env:PathsToCheckFor.Split(';').Length;
-    $env:PATH = [String]::Join(';', $PathArray);
-  }
 
-  Function Resolve-DictionaryPath {
-    param(
-      # Specifies a path to one or more locations. Unlike the Path parameter, the value of the LiteralPath parameter is
-      # used exactly as it is typed. No characters are interpreted as wildcards. If the path includes escape characters,
-      # enclose it in single quotation marks. Single quotation marks tell Windows PowerShell not to interpret any
-      # characters as escape sequences.
-      [Parameter(ParameterSetName = 'LiteralPath',
-        Mandatory = $true,
-        ValueFromPipelineByPropertyName = $true,
-        Position = 0,
-        HelpMessage = "Literal path to one or more locations.")]
-      [Alias('PSPath', 'LP')]
-      [ValidateNotNullOrEmpty()]
-      [string]
-      ${LiteralPath}
-    )
+    Try {
+      $OutBuffer = $Null
+      if ($PSBoundParameters.TryGetValue("OutBuffer", [ref]$OutBuffer)) {
+        $PSBoundParameters["OutBuffer"] = 1
+      }
 
-    $PathDirectories = $LiteralPath.Split('\');
-    if ($PathDirectories -contains "TexTools" -and $PathDirectories -contains "ModPacks") {
-      Add-ToPath -LiteralPath @("$($env:FFXIV)\TexTools");
+      $GetPath = $Null;
+      if ($PSBoundParameters.TryGetValue("Path", [ref]$GetPath)) {
+        $Temp_Path = (Get-Item -LiteralPath (Resolve-Path -LiteralPath $GetPath));
+        Write-Debug $Temp_Path
+        Resolve-DictionaryPath -LiteralPath $Temp_Path;
+      }
+      $WrappedCmd = $ExecutionContext.InvokeCommand.GetCommand("Microsoft.PowerShell.Management\Set-Location", [System.Management.Automation.CommandTypes]::Cmdlet)
+      $ScriptCmd = { & $WrappedCmd @PSBoundParameters }
+
+      $SteppablePipeline = $ScriptCmd.GetSteppablePipeline($MyInvocation.CommandOrigin)
+      $SteppablePipeline.Begin($PSCmdlet)
     }
-    else {
-      Clear-AddedPaths;
+    Catch {
+      Throw;
     }
   }
-
-  try {
-    $outBuffer = $null
-    if ($PSBoundParameters.TryGetValue('OutBuffer', [ref]$outBuffer)) {
-      $PSBoundParameters['OutBuffer'] = 1
+  Process {
+    Try {
+      $SteppablePipeline.Process($_);
     }
-
-    $getPath = $null;
-    if ($PSBoundParameters.TryGetValue('Path', [ref]$getPath)) {
-      $path = (Get-Item -LiteralPath (Resolve-Path -LiteralPath $getPath));
-      Write-Debug $path
-      Resolve-DictionaryPath -LiteralPath $path;
+    Catch {
+      Throw;
     }
-    $wrappedCmd = $ExecutionContext.InvokeCommand.GetCommand('Microsoft.PowerShell.Management\Set-Location', [System.Management.Automation.CommandTypes]::Cmdlet)
-    $scriptCmd = { & $wrappedCmd @PSBoundParameters }
-
-    $steppablePipeline = $scriptCmd.GetSteppablePipeline($myInvocation.CommandOrigin)
-    $steppablePipeline.Begin($PSCmdlet)
   }
-  catch {
-    throw
+  End {
+    Try {
+      $SteppablePipeline.End();
+      If ($Null -ne $SteppablePipeline) {
+        $SteppablePipeline.Clean();
+      }
+    }
+    Catch {
+      throw;
+    }
   }
+  <#
+  Clean {
+    If ($Null -ne $SteppablePipeline) {
+      $SteppablePipeline.Clean();
+    }
+  }
+  #>
 }
 
-process {
-  try {
-    $steppablePipeline.Process($_)
-  }
-  catch {
-    throw
-  }
-}
-
-end {
-  try {
-    $steppablePipeline.End()
-  }
-  catch {
-    throw
-  }
-}
-
-clean {
-  if ($null -ne $steppablePipeline) {
-    $steppablePipeline.Clean()
-  }
+If ($PSVersionTable.PSVersion.Major -le 5) {
+  Invoke-ForFive
+} Else {
+  Invoke-ForSeven
 }
 <#
 

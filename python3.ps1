45e546f08618c981566d987eff732481ec4f50ac
thakyZ <950594+thakyZ@users.noreply.github.com>
2024-02-22 07:03:38 -0800

    update: python3 proxy script

--- python3.ps1
+++ python3.ps1
@@ -5,16 +5,46 @@ If (-not (Test-Path -LiteralPath $PointDirectory -PathType Leaf)) {
 }
 
 ForEach ($Line In (Get-Content -LiteralPath (Join-Path -Path $PSScriptRoot -ChildPath "python3_bin_reldir.txt"))) {
-  $script:PYTHON_BIN_ABSDIR = $Line;
+  $script:Python_Bin_AbsDir = $Line;
   Break;
 }
 
-If (-not (Test-Path -LiteralPath $script:PYTHON_BIN_ABSDIR -PathType Container)) {
-  Throw "$($script:PYTHON_BIN_ABSDIR) does not exist on the drive.";
+If (-not (Test-Path -LiteralPath $script:Python_Bin_AbsDir -PathType Container)) {
+  Throw "$($script:Python_Bin_AbsDir) does not exist on the drive.";
 }
 
-$env:PATH = ("$script:PYTHON_BIN_ABSDIR;" + (Join-Path -Path $script:PYTHON_BIN_ABSDIR -ChildPath "Scripts") + ";$($env:PATH)");
+$env:PATH = ("$script:Python_Bin_AbsDir;" + (Join-Path -Path $script:Python_Bin_AbsDir -ChildPath "Scripts") + ";$($env:PATH)");
 
-& "python" $args;
+$Python = (Get-Command -Name "python");
+
+If ($Null -eq $Python) {
+  Throw "Failed to find python executable on path."
+}
+
+$PythonVersion = (& "$($Python.Source)" "--version");
+
+$NewArgs = $Args;
+If ($Args -contains "-Debug" -or $Args -contains "-debug") {
+  $DebugPreference = "Continue";
+  ForEach ($Arg in $Args) {
+    If ($Arg.ToLower() -ne "-debug") {
+      $NewArgs += @($Arg);
+    }
+  }
+}
+
+If ($DebugPreference -ne "SilentlyContinue") {
+  Write-Host -ForegroundColor Blue -Object "Using Python Version $($PythonVersion.Replace('Python', ''))";
+}
+
+If ((Get-Item -LiteralPath $Python.Source).Directory.FullName -ne $script:Python_Bin_AbsDir -and -not [System.String]::IsNullOrEmpty($script:Python_Bin_AbsDir)) {
+  Write-Warning -Message "Python source does not match what is in the config file.`nGot $((Get-Item -LiteralPath $Python.Source).Directory.FullName) Expected $($script:Python_Bin_AbsDir)";
+}
+
+& "$($Python.Source)" ( -join " ");
+
+$ExitCode = $LASTEXITCODE;
 
 Clear-Variable -Name "PYTHON_BIN_ABSDIR" -Scope Script;
+
+Exit $ExitCode;

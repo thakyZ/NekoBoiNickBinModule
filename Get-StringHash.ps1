8da7df6394564d04a67ac92b09fa2cc33d58fa7e
thakyZ <950594+thakyZ@users.noreply.github.com>
2024-02-22 07:04:23 -0800

    update: get hash of string script

--- Get-StringHash.ps1
+++ Get-StringHash.ps1
@@ -1,15 +1,37 @@
-param
-(
-  [String] $String,
-  $HashName = "MD5"
+[CmdletBinding()]
+Param(
+  # Specifies a string to convert.
+  [Parameter(Mandatory = $False,
+             ValueFromPipelineByPropertyName = $True,
+             ValueFromRemainingArguments = $True,
+             HelpMessage = "A string to convert.")]
+  [ValidateNotNullOrEmpty()]
+  [System.String]
+  $String,
+  # Specifies the hashing algorithm to use. Defaults to 'SHA256'.
+  [Parameter(Mandatory = $False,
+             HelpMessage = "The hashing algorithm to use. Defaults to 'SHA256'.")]
+  [ValidateSet("MD5", "SHA1", "SHA256", "SHA384", "SHA512")]
+  [System.String]
+  $Algorithm = "SHA256"
 )
-$bytes = [System.Text.Encoding]::UTF8.GetBytes($String)
-$algorithm = [System.Security.Cryptography.HashAlgorithm]::Create('MD5')
-$StringBuilder = New-Object System.Text.StringBuilder
 
-$algorithm.ComputeHash($bytes) |
-ForEach-Object {
-  $null = $StringBuilder.Append($_.ToString("x2"))
+Begin {
+  [System.Byte[]]$Bytes = [System.Text.Encoding]::UTF8.GetBytes($String)
+  $AlgorithmObj = [System.Security.Cryptography.HashAlgorithm]::Create($Algorithm)
+  $StringBuilder = (New-Object -TypeName System.Text.StringBuilder);
 }
-
-$StringBuilder.ToString()
+Process {
+  [System.Byte[]]$ByteString = $AlgorithmObj.ComputeHash($Bytes);
+  ForEach ($Byte in $ByteString) {
+    $Null = $StringBuilder.Append($Byte.ToString("x2"));
+  }
+}
+End {
+  $Output = @{
+    Algorithm = $Algorithm;
+    Hash = $StringBuilder.ToString();
+    String = $String;
+  };
+  Write-Output -InputObject $Output;
+}

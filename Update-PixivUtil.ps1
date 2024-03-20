b82e96a245f891c5c507a7f8dabfe384d47983e9
thakyZ <950594+thakyZ@users.noreply.github.com>
2024-03-20 10:52:36 -0700

    update: update-pixivutil tool

--- Update-PixivUtil.ps1
+++ Update-PixivUtil.ps1
@@ -1,35 +1,72 @@
-param()
+[CmdletBinding()]
+Param()
 
-$repo = "Nandaka/PixivUtil2"
+$Repository = "Nandaka/PixivUtil2";
 
-$releases = "https://api.github.com/repos/${repo}/releases"
+$Releases = "https://api.github.com/repos/$($Repository)/releases";
 
-$DownloadDest = "$($env:APROG_DIR)\Pixivutil"
+$DownloadDest = "$($env:APROG_DIR)\Pixivutil";
 
-Write-Host "Determining latest release..."
-$repoData = (Invoke-WebRequest "${releases}" | ConvertFrom-Json)[0].assets;
-$tagName = (Invoke-WebRequest "${releases}" | ConvertFrom-Json)[0].tag_name;
+. "$((Join-Path -Path (Get-Item -Path $Profile).Directory -ChildPath "Utils.ps1"))";
 
-$filesToDownloadApi = @()
-$filesToDownload = @()
-$fileNames = @()
+$script:Config = (Get-Config -Path (Join-Path -Path $PSScriptRoot -ChildPath "config.json"));
 
-$repoData | ForEach-Object { $filesToDownloadApi += $_.url }
+$FoundTokens = ($script:Config.Tokens | Where-Object {
+    $Obj1 = $_;
+    Return $Null -ne ($Obj1.Addresses | Where-Object {
+        $Obj2 = $_;
+        Return $Releases -match $Obj2;
+      });
+  });
 
-$filesToDownloadApi | ForEach-Object {
-  $data = (Invoke-WebRequest $_ | ConvertFrom-Json)
-  $filesToDownload += $data.browser_download_url
-  $fileNames += $data.name
+Remove-Variable -Name "Config" -Scope Script;
+$script:Bearer = $Null;
+If ($Null -ne $FoundTokens) {
+  $script:Bearer = (ConvertTo-SecureString -String $FoundTokens.Token -AsPlainText);
 }
 
-Write-Host "Latest release: $($tagName)";
-
-Write-Host "Dowloading latest release..."
-
-for ($i = 0; $i -lt $filesToDownload.length; $i++) {
-  $fileName = $fileNames[$i]
-  Invoke-WebRequest $filesToDownload[$i] -Out "${DownloadDest}\${fileName}"
+Write-Host "Determining latest release...";
+[System.Object[]]$ReleasesData = $null
+[System.Object]$RepositoryData = $Null;
+[System.String]$TagName = $Null;
+Try {
+  $ReleasesData = (Invoke-RestMethod -Uri "$($Releases)" -UserAgent ([Microsoft.PowerShell.Commands.PSUserAgent]::Chrome) -SkipHttpErrorCheck -Authentication Bearer -Token $script:Bearer);
+} Catch {
+  Throw;
 }
+$RepositoryData = $ReleasesData[0].assets;
+$TagName = $ReleasesData[0].tag_name;
+
+$FilesToDownloadApi = @()
+$FilesToDownload = @()
+$FileNames = @()
+
+$RepositoryData | ForEach-Object { $FilesToDownloadApi += $_.url }
+
+ForEach ($Item in $FilesToDownloadApi) {
+  $Data = (Invoke-RestMethod -Uri "$($Item)" -UserAgent ([Microsoft.PowerShell.Commands.PSUserAgent]::Chrome) -SkipHttpErrorCheck -Authentication Bearer -Token $script:Bearer);
+  $FilesToDownload += $Data.browser_download_url
+  $FileNames += $Data.name
+}
+
+Write-Host "Latest release: $($TagName)";
+
+Write-Host "Downloading latest release..."
+
+For ($Index = 0; $Index -lt $filesToDownload.length; $Index++) {
+  $FileName = $FileNames[$Index];
+  Try {
+    $WebRequest = (Invoke-WebRequest $FilesToDownload[$Index] -SkipHttpErrorCheck -Headers @{ Authentication = "Bearer $($FoundTokens.Token)"} -UserAgent ([Microsoft.PowerShell.Commands.PSUserAgent]::Chrome) -OutFile "$($DownloadDest)\$($FileName)" -PassThru);
+
+    If ($WebRequest.StatusCode -ne 200) {
+      Throw "Failed to get file at $($FilesToDownload[$Index]) and returned error code $($WebRequest.StatusCode)";
+    }
+  } Catch {
+    Throw;
+  }
+}
+Remove-Variable -Name "Bearer" -Scope Script;
+Remove-Variable -Name "FoundTokens";
 
 $FilesToKeep = @(
   "Downloads",
@@ -38,28 +75,41 @@ $FilesToKeep = @(
   "db.sqlite",
   "cacert.pem",
   "pixivutil\d+.zip"
-)
+);
 
-function Test-MatchFilesToKeep() {
-  param(
-    [string] $File
+Function Test-MatchFilesToKeep() {
+  [CmdletBinding()]
+  Param(
+    # Specifies the file to check for.
+    [Parameter(Mandatory = $True,
+               Position = 0,
+               HelpMessage = "The file to check for.")]
+    [System.String]
+    $File,
+    # Specifies the list of files to check against.
+    [Parameter(Mandatory = $True,
+               Position = 1,
+               HelpMessage = "The list of files to check against.")]
+    [System.String[]]
+    $FilesToKeep
   )
-  $j = 0;
-  for ($i = 0; $i -lt $FilesToKeep.Length; $i++) {
-    if ($File -notmatch $FilesToKeep[$i]) {
-      $j += 1;
+  $MatchedIndex = 0;
+  For ($Index = 0; $Index -lt $FilesToKeep.Length; $Index++) {
+    If ($File -notmatch $FilesToKeep[$Index]) {
+      $MatchedIndex += 1;
     }
   }
-  if ($j -eq $FilesToKeep.Length) {
-    return $True;
-  }
-  return $False;
+  Return ($MatchedIndex -eq $FilesToKeep.Length);
 }
 
-Get-ChildItem -Path $DownloadDest -Depth 0 -Recurse |
-Where-Object { Test-MatchFilesToKeep -File $_.Name } |
-ForEach-Object { Remove-Item -Force -Recurse -Path $_.FullName }
+$Items = (Get-ChildItem -Path $DownloadDest -Depth 0 -Recurse | Where-Object {
+  Return (Test-MatchFilesToKeep -File $_.Name -FilesToKeep $FilesToKeep);
+});
 
-Expand-Archive -Force -Path "$($DownloadDest)\$($fileNames[0])" -DestinationPath $DownloadDest
+ForEach ($Item in $Items) {
+  Remove-Item -Force -Recurse -Path $Item.FullName;
+}
 
-Remove-Item -Force -Path "$($DownloadDest)\$($fileNames[0])"
+Expand-Archive -Force -Path "$($DownloadDest)\$($FileNames[0])" -DestinationPath $DownloadDest
+
+Remove-Item -Force -Path "$($DownloadDest)\$($FileNames[0])"

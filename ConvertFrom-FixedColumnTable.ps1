76acdaf7ee4a5291da25618d7b9ae7dcdbe7b047
thakyZ <950594+thakyZ@users.noreply.github.com>
2024-03-23 08:22:09 -0700

    update: fix ConvertFrom-FixedColumnTable

--- ConvertFrom-FixedColumnTable.ps1
+++ ConvertFrom-FixedColumnTable.ps1
@@ -7,49 +7,64 @@
 #  * The header line is assumed to be followed by a separator line
 #    (its format doesn't matter).
 #function ConvertFrom-FixedColumnTable {
-[CmdletBinding()]
-[OutputType([Object[]])]
+[CmdletBinding(PositionalBinding = $True, DefaultParameterSetName = "Default")]
+[OutputType([System.Object[]])]
 Param(
   # Specifies the input object to convert into a powershell table.
-  [Parameter(ValueFromPipeline = $True,
-    HelpMessage = "The input object to convert into a powershell table.`n" +
-    "Note:`n" +
-    " * Accepts input only via the pipeline, either line by line,`n" +
-    "   or as a single, multi-line string.`n" +
-    " * The input is assumed to have a header line whose column names`n" +
-    "   mark the start of each field`n" +
-    "   * Column names are assumed to be *single words* (must not contain spaces).`n" +
-    " * The header line is assumed to be followed by a separator line`n" +
-    "   (its format doesn't matter).")]
-  [String[]]
-  $InputObject
+  [Parameter(Mandatory = $True,
+             Position = 0,
+             ParameterSetName = "Default",
+             ValueFromPipeline = $True,
+             HelpMessage = "The input object to convert into a powershell table.`n" +
+             "Note:`n" +
+             " * Accepts input only via the pipeline, either line by line,`n" +
+             "   or as a single, multi-line string.`n" +
+             " * The input is assumed to have a header line whose column names`n" +
+             "   mark the start of each field`n" +
+             "   * Column names are assumed to be *single words* (must not contain spaces).`n" +
+             " * The header line is assumed to be followed by a separator line`n" +
+             "   (its format doesn't matter).")]
+  [ValidateNotNull()]
+  [System.Object]
+  $InputObject,
+  # Specifies a switch to use an alternative package for building the table.
+  [Parameter(Mandatory = $False,
+             ParameterSetName = "Default",
+             HelpMessage = "A switch to use an alternative package for building the table.`n" +
+             "⚠️ WARNING: The package may not be installed or may not have the expected output.")]
+  [Alias("Alt", "Alternative")]
+  [switch]
+  $AlternativeBuilder = $False
 )
 
 Begin {
   If ($Null -eq $InputObject -or $InputObject.Length -eq 0) {
+    Write-Host "IsNull: $($Null -eq $InputObject)";
+    Write-Host "Length: $($InputObject.Length)";
+    Write-Host "InputObject: $($InputObject)";
     Write-Error -Message "Argument InputObject must not be null or an empty array.";
     Exit 1;
   }
 
-  #region Process InputObject from object or string or object array or object string;
+  # region Process InputObject from object or string or object array or object string;
 
-  $Lines = @();
+  $script:Lines = @();
 
-  If ($InputObject.GetType() -eq [string[]] -or $InputObject.GetType() -eq [object[]]) {
+  If ($InputObject.GetType() -eq [System.String[]] -or $InputObject.GetType() -eq [System.Object[]]) {
     If ($InputObject.Length -eq 1 -and $InputObject[0].Contains("`n")) {
-      $Lines = [Regex]::Split($InputObject[0].TrimEnd("`r", "`n"), '\r?\n');
+      $script:Lines = [System.Text.RegularExpressions.Regex]::Split($InputObject[0].TrimEnd("`r", "`n"), '\r?\n');
     } Else {
-      $Lines = $InputObject;
+      $script:Lines = $InputObject;
     }
-  } ElseIf ($InputObject.GetType() -eq [string]) {
-    $Lines = [Regex]::Split($InputObject.TrimEnd("`r", "`n"), '\r?\n');
+  } ElseIf ($InputObject.GetType() -eq [System.String]) {
+    $script:Lines = [System.Text.RegularExpressions.Regex]::Split($InputObject.TrimEnd("`r", "`n"), '\r?\n');
   } Else {
-    $Lines = @("$($InputObject)");
+    $script:Lines = @("$($InputObject)");
   }
 
-  #endregion
+  # endregion
 
-  #region Initialize fields
+  # region Initialize fields
 
   Set-StrictMode -Version 1;
   $LineNdx = 0;
@@ -57,71 +72,73 @@ Begin {
   # [PSCustomObject[]] $Output = @();
 
 
-  #endregion
+  # endregion
 
-  #region Turnicate lines above table header
+  # region Turnicate lines above table header
 
-  $FoundHeader = 0;
-  $FoundHeaderIndex = 0;
+  $script:FoundHeader = 0;
+  $script:FoundHeaderIndex = 0;
   $Null = ($Lines | Where-Object {
       $FoundHeaderOutput = $False;
       $Temp = ($_ -Split "");
       $Temp2 = ($Temp | Where-Object { $_ -eq "-" });
       If ($Temp2.Length -eq $_.Length) {
-        $FoundHeader = $FoundHeaderIndex;
+        $script:FoundHeader = $script:FoundHeaderIndex;
         $FoundHeaderOutput = $True;
       }
-      $FoundHeaderIndex++;
+      $script:FoundHeaderIndex++;
       Return $FoundHeaderOutput;
     });
-  $FoundHeader = $FoundHeader - 1;
+  $script:FoundHeader = $script:FoundHeader - 1;
 
-  $FoundHeaderIndex = 0;
+  $script:FoundHeaderIndex = 0;
   $Lines = ($Lines | Where-Object {
-    If ($FoundHeaderIndex -ge $FoundHeader) {
-      Return $True;
-    }
-    $FoundHeaderIndex++;
-    Return $False;
-  });
+      If ($script:FoundHeaderIndex -ge $script:FoundHeader) {
+        Return $True;
+      }
+      $script:FoundHeaderIndex++;
+      Return $False;
+    });
 
-  #endregion
+  # endregion
 }
 Process {
-  #region Process lines
-
-  $Output = (($Lines -Join "`n") | ConvertFrom-SourceTable)
-
-  <#ForEach ($Line in $Lines) {
-    $LineNdx++;
-    If ($LineNdx -eq 1) {
-      # header line
-      $HeaderLine = $Line
-    } ElseIf ($LineNdx -eq 2) {
-      # separator line
-      # Get the indices where the fields start.
-      $HeaderLineMatches = [regex]::Matches($HeaderLine, '\b\S');
-      If ($HeaderLineMatches.Length -gt 0) {
-        $FieldStartIndices = $HeaderLineMatches.Index;
-      }
-      # Calculate the field lengths.
-      $FieldLengths = ForEach ($Index in 1..($FieldStartIndices.Count - 1)) {
-        $FieldStartIndices[$Index] - $FieldStartIndices[$Index - 1] - 1;
-      }
-      # Get the column names
-      $ColNames = ForEach ($Index in 0..($FieldStartIndices.Count - 1)) {
-        If ($Index -eq $FieldStartIndices.Count - 1) {
-          $HeaderLine.Substring($FieldStartIndices[$Index]).Trim()
-        } Else {
-          $HeaderLine.Substring($FieldStartIndices[$Index], $FieldLengths[$Index]).Trim()
+  # region Process lines
+  If ($AlternativeBuilder) {
+    $Output = (($script:Lines -join "`n") | ConvertFrom-SourceTable)
+  } Else {
+    ForEach ($Line in $script:Lines) {
+      $LineNdx++;
+      If ($LineNdx -eq 1) {
+        # header line
+        $HeaderLine = $Line
+      } ElseIf ($LineNdx -eq 2) {
+        # separator line
+        # Get the indices where the fields start.
+        $HeaderLineMatches = [System.Text.RegularExpressions.Regex]::Matches($HeaderLine, '\b\S');
+        If ($HeaderLineMatches.Length -gt 0) {
+          $FieldStartIndices = $HeaderLineMatches.Index;
         }
-      }
-    } Else {
-      # data line
-      $OrderedHashTable = [Ordered] @{} # ordered helper hashtable for object constructions.
-      $Index = 0
-      ForEach ($ColName in $ColNames) {
-        $OrderedHashTable[$ColName] =
+        # Calculate the field lengths.
+        $FieldLengths = @();
+        ForEach ($Index in 1..($FieldStartIndices.Count - 1)) {
+          $FieldLengths += @($FieldStartIndices[$Index] - $FieldStartIndices[$Index - 1] - 1);
+        }
+        # Get the column names
+        $ColNames = @();
+        ForEach ($Index in 0..($FieldStartIndices.Count - 1)) {
+          If ($Index -eq $FieldStartIndices.Count - 1) {
+            $ColNames += @($HeaderLine.Substring($FieldStartIndices[$Index]).Trim());
+          } Else {
+            $ColNames += @($HeaderLine.Substring($FieldStartIndices[$Index], $FieldLengths[$Index]).Trim());
+          }
+        }
+      } Else {
+        # data line
+        $OrderedHashTable = [Ordered] @{}; # ordered helper hashtable for object constructions.
+        $Index = 0
+        ForEach ($ColName in $ColNames) {
+          $OrderedHashTable[$ColName] =
           If ($FieldStartIndices[$Index] -lt $Line.Length) {
             If ($FieldLengths[$Index] -and $FieldStartIndices[$Index] + $FieldLengths[$Index] -le $Line.Length) {
               $Line.Substring($FieldStartIndices[$Index], $FieldLengths[$Index]).Trim();
@@ -129,50 +146,43 @@ Process {
               $Line.Substring($FieldStartIndices[$Index]).Trim();
             }
           }
-        $Index++;
-      }
-
-      Function Test-AllColumnsFilled() {
-        Param(
-          # Parameter help description
-          [Parameter(Mandatory = $True)]
-          [PSCustomObject]
-          $InputObject
-        )
-
-        $LocalOutput = $True;
-        [PSCustomObject[]] $NoteProperties = @(($InputObject.PSObject.Properties | Where-Object { $_.MemberType -eq "NoteProperty" }));
-
-        If (($NoteProperties | Where-Object {
-          If ([string]::IsNullOrEmpty($ToTest.Value)) {
-            Return $True;
-          }
-          Return $False;
-        }).Length -gt 0) {
-          $LocalOutput = $False;
+          $Index++;
         }
 
-        Return $LocalOutput;
-      }
+        Function Test-AllColumnsFilled() {
+          Param(
+            # Parameter help description
+            [Parameter(Mandatory = $True)]
+            [PSCustomObject]
+            $InputObject
+          )
 
-      If (-not (Test-AllColumnsFilled -InputObject $OrderedHashTable)) {
-        Write-Output $OrderedHashTable | Out-Host;
-        Continue;
+          $LocalOutput = $True;
+          [PSCustomObject[]] $NoteProperties = @(($InputObject.PSObject.Properties | Where-Object { $_.MemberType -eq "NoteProperty" }));
+
+          If (($NoteProperties | Where-Object { [System.String]::IsNullOrEmpty($ToTest.Value) }).Length -gt 0) {
+            $LocalOutput = $False;
+          }
+
+          Return $LocalOutput;
+        }
+
+        If (-not (Test-AllColumnsFilled -InputObject $OrderedHashTable)) {
+          Write-Output $OrderedHashTable | Out-Host;
+          Continue;
+        }
+        # Convert the ordered helper hashtable to an object and output it.
+        $Output += @([PSCustomObject] $OrderedHashTable);
       }
-      # Convert the ordered helper hashtable to an object and output it.
-      $Output += @([PSCustomObject] $OrderedHashTable);
     }
-  }#>
-
+  }
   #end region
 }
 End {
-  #region finish
-
+  # region finish
   # Clean up strict mode
   Set-StrictMode -Off;
-  $Output | Format-Table | Write-Output;
-
-  #endregion
+  $Output | Write-Output;
+  # endregion
 }
 #}

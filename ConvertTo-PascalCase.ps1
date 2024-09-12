ae70b0d2595d9fe1331db6f64f4d1fc6abf1c2fb
thakyZ <950594+thakyZ@users.noreply.github.com>
2024-09-12 07:53:19 -0700

    add: script to convert a script to different casings.

---
+++ ConvertTo-PascalCase.ps1
@@ -0,0 +1,33 @@
+[CmdletBinding()]
+[OutputType([System.String])]
+Param(
+  # Specifies a string to convert to PascalCase
+  [Parameter(Mandatory = $True,
+             Position = 0,
+             ValueFromPipeline = $True,
+             HelpMessage = "A string to convert to PascalCase.")]
+  [ValidateNotNullOrEmpty()]
+  [System.String]
+  $String,
+  # Specifies a culture to use. Is CurrentUICulture by default.
+  [Parameter(Mandatory = $False,
+             HelpMessage = "A culture to use. Is CurrentUICulture by default.")]
+  [AllowNull()]
+  [System.Globalization.CultureInfo]
+  $Culture
+)
+
+Begin {
+  [System.Text.RegularExpressions]$CaseMatchRegex = [System.Text.RegularExpressions]::new("(?:([A-Z]?[a-z]+)[ _-]?)+");
+  [System.Text.StringBuilder]     $Output         = [System.Text.StringBuilder]::new();
+} Process {
+  ForEach ($Match in $CaseMatchRegex.Matches($String)) {
+    ForEach ($Group in $Match.Groups.Values) {
+      If ($Group.Value -ne $String) {
+        $Output.Append((ConvertTo-TitleCase -String $Group -Culture $Culture));
+      }
+    }
+  }
+} End {
+  Return $Output.ToString();
+}
---
+++ ConvertTo-TitleCase.ps1
@@ -0,0 +1,29 @@
+[CmdletBinding()]
+[OutputType([System.String])]
+Param(
+  # Specifies a string to convert to Title Case
+  [Parameter(Mandatory = $True,
+             Position = 0,
+             ValueFromPipeline = $True,
+             HelpMessage = "A string to convert to Title Case.")]
+  [ValidateNotNullOrEmpty()]
+  [System.String]
+  $String,
+  # Specifies a culture to use. Is CurrentUICulture by default.
+  [Parameter(Mandatory = $False,
+             HelpMessage = "A culture to use. Is CurrentUICulture by default.")]
+  [AllowNull()]
+  [System.Globalization.CultureInfo]
+  $Culture
+)
+
+Begin {
+  [System.String]$Output = $String;
+} Process {
+  If ($Null -eq $Culture) {
+    $Culture = [System.Globalization.CultureInfo]::CurrentUICulture;
+  }
+  $Output = $Culture.TextInfo.ToTitleCase(($String.ToLower()));
+} End {
+  Return $Output;
+}

dd626088d5e69e1d02cbbb79340576b809fa3f9a
thakyZ <950594+thakyZ@users.noreply.github.com>
2024-09-12 07:51:27 -0700

    add: script to convert from regex named group's capture value

---
+++ ConvertFrom-RegexNamedGroupCapture.ps1
@@ -0,0 +1,27 @@
+[CmdletBinding()]
+Param (
+  [Parameter(Mandatory = $True,
+             ValueFromPipeline = $True,
+             ValueFromPipelineByPropertyName = $True,
+             Position = 0)]
+  [System.Text.RegularExpressions.Match]
+  $Match,
+  [Parameter(Mandatory = $True,
+             ValueFromPipelineByPropertyName = $True,
+             Position = 1)]
+  [System.Text.RegularExpressions.Regex]
+  $Regex
+)
+Process {
+  If (-not $Match.Groups[0].Success) {
+    Throw [System.ArgumentException]::new("Match does not contain any captures.", "Match");
+  }
+  $H = @{}
+  ForEach ($Name in $Regex.GetGroupNames()) {
+    If ($Name -eq 0) {
+      Continue;
+    }
+    $H["$($Name)"] = $Match.Groups["$($Name)"].Value
+  }
+  Return $H
+}

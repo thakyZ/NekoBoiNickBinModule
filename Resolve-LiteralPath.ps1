fb225f6cf7fcaeb63303f754d47a1e1485601161
thakyZ <950594+thakyZ@users.noreply.github.com>
2024-10-27 07:48:55 -0700

    add: wip script to resolve literal path from a root and a relative path.

---
+++ Resolve-LiteralPath.ps1
@@ -0,0 +1,39 @@
+[CmdletBinding()]
+[OutputType([System.String])]
+Param(
+    # TODO: Parameter help description
+    [Parameter(Mandatory = $False,
+               Position = 0,
+               HelpMessage = "TBA")]
+    [System.Management.Automation.PSCredential]
+    $Credential,
+    # TODO: Parameter help description
+    [Parameter(Mandatory = $True,
+               Position = 1,
+               HelpMessage = "TBA")]
+    [Alias("Path","PSPath")]
+    [ValidateNotNullOrEmpty()]
+    [System.String[]]
+    $LiteralPath,
+    # TODO: Parameter help description
+    [Parameter(Mandatory = $False,
+               Position = 2,
+               HelpMessage = "TBA")]
+    [Switch]
+    $Relative,
+    # TODO: Parameter help description
+    [Parameter(Mandatory = $False,
+               Position = 3,
+               HelpMessage = "TBA")]
+    [Alias("RelativeBasePath","BasePath")]
+    [System.String]
+    $RelativeBaseLiteralPath
+)
+
+Begin {
+    $EscapedBasePath = ($RelativeBaseLiteralPath -replace '([[\]])','`$1');
+} Process {
+    [System.IO.Path]::GetRelativePath("$($Compile[0].ProviderPath1)","$($Compile[0].FullName)")
+} End {
+
+}

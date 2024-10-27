c6beab2bdb7a670006bfbabaeb63ca0d465d0f0e
thakyZ <950594+thakyZ@users.noreply.github.com>
2024-10-27 08:08:43 -0700

    update: script Unblock-Files

--- Unblock-Files.ps1
+++ Unblock-Files.ps1
@@ -1,124 +1,112 @@
+[CmdletBinding()]
 Param (
   # Specifies a path to one or more locations.
-  [Parameter(Mandatory = $true,
+  [Parameter(Mandatory = $False,
     Position = 0,
     ParameterSetName = "Recursive",
-    ValueFromPipeline = $true,
-    ValueFromPipelineByPropertyName = $true,
+    ValueFromPipeline = $True,
+    ValueFromPipelineByPropertyName = $True,
     HelpMessage = "Path to one or more locations.")]
-  [Parameter(Mandatory = $true,
+  [Parameter(Mandatory = $False,
     Position = 0,
     ParameterSetName = "NonRecursive",
-    ValueFromPipeline = $true,
-    ValueFromPipelineByPropertyName = $true,
+    ValueFromPipeline = $True,
+    ValueFromPipelineByPropertyName = $True,
     HelpMessage = "Path to one or more locations.")]
   [Alias("PSPath", "LiteralPath")]
-  [ValidateNotNullOrEmpty()]
-  [string[]]
-  $Path,
+  [System.String[]]
+  $Path = @($PWD),
   # Recurse directory path.
-  [Parameter(Mandatory = $true,
+  [Parameter(Mandatory = $True,
     ParameterSetName = "Recursive",
     HelpMessage = "Recurse directory path.")]
   [switch]
   $Recurse,
   # Recurse directory path.
-  [Parameter(Mandatory = $false,
+  [Parameter(Mandatory = $False,
     ParameterSetName = "Recursive",
     HelpMessage = "Recurse directory path.")]
-  [int]
-  $Depth = -1,
+  [System.UInt32]
+  $Depth = [System.UInt32]::MaxValue,
   # Recurse directory path.
-  [Parameter(Mandatory = $false,
+  [Parameter(Mandatory = $False,
     ParameterSetName = "Recursive",
     HelpMessage = "Recurse directory path.")]
-  [Parameter(Mandatory = $false,
+  [Parameter(Mandatory = $False,
     ParameterSetName = "NonRecursive",
     HelpMessage = "Recurse directory path.")]
-  [string]
-  $Filter = "",
+  [System.String]
+  $Filter = $Null,
   # Recurse directory path.
-  [Parameter(Mandatory = $false,
+  [Parameter(Mandatory = $False,
     ParameterSetName = "Recursive",
     HelpMessage = "Recurse directory path.")]
-  [Parameter(Mandatory = $false,
+  [Parameter(Mandatory = $False,
     ParameterSetName = "NonRecursive",
     HelpMessage = "Recurse directory path.")]
-  [string[]]
+  [System.String[]]
   $Include = @(),
   # Recurse directory path.
-  [Parameter(Mandatory = $false,
+  [Parameter(Mandatory = $False,
     ParameterSetName = "Recursive",
     HelpMessage = "Recurse directory path.")]
-  [Parameter(Mandatory = $false,
+  [Parameter(Mandatory = $False,
     ParameterSetName = "NonRecursive",
     HelpMessage = "Recurse directory path.")]
-  [string[]]
+  [System.String[]]
   $Exclude = @()
 )
 
 Begin {
-  $CommandBuilt = "Get-ChildItem -LiteralPath `$Path";
-
-  $PathType = $null;
-
-  if (Test-Path -LiteralPath $Path -PathType Container) {
-    $PathType = "Directory";
-  }
-  elseif (Test-Path -LiteralPath $Path -PathType Leaf) {
-    $PathType = "File";
-  }
-
-  if ($null -eq $PathType) {
-    Write-Error "Path specified is not valid";
-  }
-
-  if ($PathType -eq "File" -and $Recurse -eq $true) {
-    Write-Warning "Path specified is not a directory Recursive switch does nothing.";
-  }
-  elseif ($PathType -eq "Directory" -and $Recurse -eq $true) {
-    $CommandBuilt += " -Recurse";
-
-    if ($Filter -ne "") {
-      $CommandBuilt += " -Filter `$Filter";
-    }
-    if ($Include.Length -gt 0) {
-      $CommandBuilt += " -Include `$Include";
-    }
-    if ($Include.Length -gt 0) {
-      $CommandBuilt += " -Exclude `$Exclude";
-    }
-    if ($Depth -ge 0) {
-      $CommandBuilt += " -Depth `$Depth";
-    }
-  }
-  elseif ($PathType -eq "Directory" -and $Recurse -eq $false) {
-    if ($Filter -ne "") {
-      $CommandBuilt += " -Filter `$Filter";
-    }
-    if ($Include.Length -gt 0) {
-      $CommandBuilt += " -Include `$Include";
-    }
-    if ($Include.Length -gt 0) {
-      $CommandBuilt += " -Exclude `$Exclude";
-    }
-  }
-
-  $Files = Invoke-Expression -Command $CommandBuilt;
-}
-Process {
-  ForEach ($File in $Files) {
-    if ($PSCmdlet.MyInvocation.BoundParameters["Verbose"].IsPresent) {
-      Write-Host "Unblocking File $($File.FullName)";
-    }
-
-    try {
-      Get-Item -LiteralPath "$($File.FullName)" | Unblock-File;
-    }
-    catch {
-      Write-Error -Exception $_.Exception;
+  [System.IO.FileSystemInfo[]] $Files = (Get-ChildItem -LiteralPath $Path -File -Recurse:($Recurse -eq $True) -Filter:($Filter) -Include:($Include) -Exclude:($Exclude) -Depth:($Depth));
+} Process {
+  Function Set-FileReadable {
+    [CmdletBinding()]
+    Param(
+      [ValidateNotNull()]
+      [System.IO.FileSystemInfo]
+      $File,
+      [ValidateNotNull()]
+      [System.Boolean]
+      $Read
+    )
+    Try {
+      Get-Item -LiteralPath $File -Verbose:($PSCmdlet.MyInvocation.BoundParameters["Verbose"].IsPresent -eq $True) | Set-ItemProperty -Name IsReadOnly -Value $Read -Verbose:($PSCmdlet.MyInvocation.BoundParameters["Verbose"].IsPresent -eq $True)
+    } Catch {
+      Write-Error -ErrorRecord $_;
       Exit 1;
     }
   }
+
+  Function Set-UnblockFile {
+    [CmdletBinding()]
+    Param(
+      [ValidateNotNull()]
+      [System.IO.FileSystemInfo[]]
+      $Files
+    )
+    ForEach ($File in $Files) {
+      Try {
+        Unblock-File -LiteralPath $File -Verbose:($PSCmdlet.MyInvocation.BoundParameters["Verbose"].IsPresent -eq $True);
+      } Catch {
+        If ($_.Exception.Message.EndsWith(":Zone.Identifier' is denied.")) {
+          Try {
+            Set-FileReadable -File $File -Read $True -Verbose:($PSCmdlet.MyInvocation.BoundParameters["Verbose"].IsPresent -eq $True);
+            Unblock-File -LiteralPath $File -Verbose:($PSCmdlet.MyInvocation.BoundParameters["Verbose"].IsPresent -eq $True);
+            Set-FileReadable -File $File -Read $False -Verbose:($PSCmdlet.MyInvocation.BoundParameters["Verbose"].IsPresent -eq $True);
+          } Catch {
+            Write-Error -ErrorRecord $_;
+            Exit 1;
+          }
+        } Else {
+          Write-Error -ErrorRecord $_;
+          Exit 1;
+        }
+      }
+    }
+  }
+
+  Set-UnblockFile -Files $Files;
+
   Exit 0;
 }

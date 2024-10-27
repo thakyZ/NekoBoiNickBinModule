dd83ac501309fd2282a1c4b3e91ee70ff1326556
thakyZ <950594+thakyZ@users.noreply.github.com>
2024-10-27 07:25:03 -0700

    refectored: python scripts

--- compare.py
+++ compare.py
@@ -1,46 +1,104 @@
+#!/usr/bin/env python3
+
+"""Compare Script"""
+
 import os
-import argparse
+from pathlib import Path
+import sys
+from argparse import ArgumentParser, Namespace
+from typing import Any
 
-parser = argparse.ArgumentParser(description="Outputs the hashes of all subdirectories of target.")
-parser.add_argument("--silent", action="store_true", help="Disables console output.")
-parser.add_argument("--fileA", type=str, help="Input file A.")
-parser.add_argument("--fileB", type=str, help="Input file B.")
-parser.add_argument("--output", type=str, help="Output file.")
-parser.add_argument("--open", action="store_true", help="Opens file upon completion.")
-parser.add_argument("--force", action="store_true", help="Overwrite without prompt.")
+def write(text_file: Path | None, key_values: list[tuple[bool, str, str]]):
+    """Writes the given key_values to a given file or prints if no file specified.
 
-args = parser.parse_args()
+    Args:
+        text_file (Path | None): The file to write to.
+        key_values (list[tuple[str, str]]): the given keys that were compared.
+    """
+    lines: list[str] = []
+    for value in key_values:
+        operator: str = ""
+        if value[0] is True:
+            operator = "=="
+        elif value[0] is False:
+            operator = "!="
+        lines.append(f"{value[1]} {operator} {value[2]}\n")
+    if isinstance(text_file, Path):
+        with text_file.open("w", encoding="utf8") as fw:
+            fw.writelines(lines)
+    else:
+        for line in lines:
+            print(line)
 
-pathA = args.fileA or "fileA.txt"
-pathB = args.fileB or "fileB.txt"
+def process(dict_a: dict[str, Any], dict_b: dict[str, Any]) -> list[tuple[bool, str, str]]:
+    """Processes the dictionaries and outputs them.
 
-fileA = open(pathA, "r")
-dictA = {}
-for line in fileA:
-    split = line.split(",")
-    dictA[split[0]] = split[1][:-1]
-fileA.close()
+    Args:
+        args (Namespace): Args passed to the CLI
+        dict_a (dict[str, Any]): The first dictionary.
+        dict_b (dict[str, Any]): The second dictionary.
+    """
+    key_values: list[tuple[bool, str, str]] = []
+    for key_a, value_a in dict_a.items():
+        for key_b, value_b in dict_b.items():
+            _tuple: tuple[bool, str, str]
+            if key_a == key_b and value_a != value_b:
+                _tuple = (False, key_a, key_b)
+                key_values.append(_tuple)
+            if key_a == key_b and value_a == value_b:
+                _tuple = (True, key_a, key_b)
+                key_values.append(_tuple)
+    return key_values
 
-fileB = open(pathB, "r")
-dictB = {}
-for line in fileB:
-    split = line.split(",")
-    dictB[split[0]] = split[1][:-1]
-fileA.close()
+def main() -> None:
+    """Main method"""
 
-if args.output:
-    if not args.force and os.path.exists(args.output) and input("The file \"" + args.output + "\" already exists, would you like to overwrite? [y/n] ").lower() != 'y':
-            quit()
-    text_file = open(args.output, "w")
+    parser = ArgumentParser(description="Outputs the hashes of all subdirectories of target.")
+    parser.add_argument("--silent", action="store_true", help="Disables console output.")
+    parser.add_argument("--fileA", type=str, help="Input file A.")
+    parser.add_argument("--fileB", type=str, help="Input file B.")
+    parser.add_argument("--output", type=str, help="Output file.")
+    parser.add_argument("--open", action="store_true", help="Opens file upon completion.")
+    parser.add_argument("--force", action="store_true", help="Overwrite without prompt.")
 
-for id,hash in dictA.items():
-    if id in dictB and dictA[id] != dictB[id]:
-        if not args.silent:
-            print(id)
-        if args.output:
-            text_file.write(id + "\n")
+    args: Namespace = parser.parse_args()
 
-if args.output:
-    text_file.close()
-    if args.open:
-        os.startfile(args.output)
+    path_a: Path = Path(args.fileA or "fileA.txt")
+    path_b: Path = Path(args.fileB or "fileB.txt")
+    dict_a: dict[str, Any] = {}
+    dict_b: dict[str, Any] = {}
+
+    with path_a.open("r", encoding="utf8") as file_a:
+        for line in file_a.readlines():
+            split = line.split(",")
+            dict_a[split[0]] = split[1][:-1]
+        file_a.close()
+
+    with path_b.open("r", encoding="utf8") as file_b:
+        for line in file_b.readlines():
+            split = line.split(",")
+            dict_b[split[0]] = split[1][:-1]
+        file_b.close()
+
+    text_file: Path | None = None
+    if args.output:
+        if (not args.force and Path(args.output).exists() and
+            input(f"The file \"{args.output}\" already exists"
+                  ", would you like to overwrite? [y/n] ").lower() != 'y'):
+            sys.exit(0)
+        else:
+            text_file = Path(args.output)
+            with text_file.open("w", encoding="utf8") as fw:
+                fw.truncate(0)
+
+    key_values: list[tuple[bool, str, str]] = process(dict_a, dict_b)
+
+    if args.output and isinstance(text_file, Path):
+        write(text_file, key_values)
+        if args.open:
+            os.startfile(args.output)
+    elif not args.silent or not args.output:
+        write(None, key_values)
+
+if __name__ == "__main__":
+    main()
--- stellaris_hasher.py
+++ stellaris_hasher.py
@@ -1,53 +1,36 @@
 #!/usr/bin/env python3
-"""Module"""
-# cSpell:ignore rootdir startfile steamapps scandir
+
+"""Stellaris Hasher Code"""
+
+# cSpell:ignore root_dir startfile steamapps scandir
 import hashlib
+from io import TextIOWrapper
 import os
-import argparse
+from argparse import ArgumentParser, Namespace
+from pathlib import Path
 import sys
 from typing import Set, cast
 import xxhash
 
-parser = argparse.ArgumentParser(
-    description="Outputs the hashes of all subdirectories of target.")
-parser.add_argument("--silent", action="store_true",
-                    help="Disables console output.")
-parser.add_argument("--rootdir", type=str, help="The target directory.")
-parser.add_argument("--file", type=str, help="Output file.")
-parser.add_argument("--single", action="store_true")
-parser.add_argument("--open", action="store_true",
-                    help="Opens file upon completion.")
-parser.add_argument("--force", action="store_true",
-                    help="Overwrite without prompt.")
-parser.add_argument("--algorithm", type=str,
-                    help="The hashing library to use.")
-
-
-args = parser.parse_args()
-
-rootdir: str = args.rootdir or \
-    "C:\\Program Files (x86)\\Steam\\steamapps\\workshop\\content\\281990"
-
-
-def get_digest(lib1: str):
+def get_digest(lib: str):
     """Method"""
-    if lib1 == "xxh32":
+    if lib == "xxh32":
         return xxhash.xxh32()
-    if lib1 == "xxh64":
+    if lib == "xxh64":
         return xxhash.xxh64()
-    return hashlib.new(lib1)
+    return hashlib.new(lib)
 
 
-def hash_directory(path: str, lib2: str):
+def hash_directory(path: Path, lib: str):
     """Method"""
-    digest = get_digest(lib2)
+    digest = get_digest(lib)
 
     for root, _, files in os.walk(path):
         for names in files:
             file_path = os.path.join(root, names)
 
             digest.update(hashlib.sha1(
-                file_path[len(path):].encode()).digest())
+                file_path[len(str(path)):].encode()).digest())
 
             if os.path.isfile(file_path):
                 with open(file_path, 'rb') as f_obj:
@@ -58,41 +41,73 @@ def hash_directory(path: str, lib2: str):
                         digest.update(buf)
     return digest.hexdigest()
 
+def scan_directory(text_file: TextIOWrapper, args: Namespace, root_dir: Path, lib: str) -> None:
+    """Method"""
+    for directory in os.scandir(root_dir):
+        line_str: str = f"{directory.name, hash_directory(Path(directory.path), lib)}"
+        if directory.is_dir():
+            if not args.silent:
+                print(line_str)
+            if args.file:
+                text_file.write(line_str + "\n")
 
-lib = args.algorithm or 'xxh64'
-
-algorithms_available: Set[str] = (cast(Set[str], hashlib.algorithms_available) - {
-    "shake_128", "shake_256"}).union({"xxh32", "xxh64"})
-
-if lib not in algorithms_available:
-    print(" ".join({
-        f"\"{lib}\" algorithm not found. Available algorithms are ",
-        f"[{', '.join(algorithms_available)}]"
-    }))
-    sys.exit(1)
-
-if args.file:
-    if not args.force and os.path.exists(args.file) and \
-            input(f"The file \"{args.file}\" already exists, would you like to overwrite? [y/n] ") \
-            .lower() != "y":
-        sys.exit(1)
+def handle_method(args: Namespace, root_dir: Path, lib: str) -> None:
+    """Method"""
     with open(args.file, "w", encoding="utf-8") as text_file:
         if args.single:
-            line = f"{os.path.basename(rootdir)},{hash_directory(rootdir, lib)}"
+            line_str: str = f"{os.path.basename(root_dir)},{hash_directory(root_dir, lib)}"
             if not args.silent:
-                print(line)
+                print(line_str)
             if args.file:
-                text_file.write(line + "\n")
+                text_file.write(line_str + "\n")
         else:
-            for dir1 in os.scandir(rootdir):
-                line = f"{dir1.name, hash_directory(dir1.path, lib)}"
-                if dir1.is_dir():
-                    if not args.silent:
-                        print(line)
-                    if args.file:
-                        text_file.write(line + "\n")
+            scan_directory(text_file, args, root_dir, lib)
 
         if args.file:
             text_file.close()
             if args.open:
                 os.startfile(args.file)
+
+def main() -> None:
+    """Method"""
+    parser = ArgumentParser(description="Outputs the hashes of all subdirectories of target.")
+    parser.add_argument("--silent", action="store_true", help="Disables console output.")
+    parser.add_argument("--root_dir", type=str, help="The target directory.")
+    parser.add_argument("--file", type=str, help="Output file.")
+    parser.add_argument("--single", action="store_true")
+    parser.add_argument("--open", action="store_true", help="Opens file upon completion.")
+    parser.add_argument("--force", action="store_true", help="Overwrite without prompt.")
+    parser.add_argument("--algorithm", type=str, help="The hashing library to use.")
+
+
+    args = parser.parse_args()
+
+    root_dir: Path = Path(args.root_dir) or \
+        Path("C:\\Program Files (x86)\\Steam\\steamapps\\workshop\\content\\281990")
+
+    if not root_dir.exists():
+        raise FileNotFoundError(f"Direcctory in {root_dir} not found.")
+
+    lib: str = args.algorithm or 'xxh64'
+
+    algorithms_available: Set[str] = (cast(Set[str], hashlib.algorithms_available) - {"shake_128", "shake_256"}).union({"xxh32", "xxh64"})
+
+    if lib not in algorithms_available:
+        print(" ".join({
+            f"\"{lib}\" algorithm not found. Available algorithms are ",
+            f"[{', '.join(algorithms_available)}]"
+        }))
+        sys.exit(1)
+
+    if args.file:
+        if (not args.force
+            and os.path.exists(args.file)
+            and input(
+                f"The file \"{args.file}\" already exists, would you like to overwrite? [y/n] ")
+                .lower() != "y"
+        ):
+            sys.exit(1)
+        handle_method(args, root_dir, lib)
+
+if __name__ == "__main__":
+    main()

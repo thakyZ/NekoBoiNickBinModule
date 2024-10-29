c5c3de5191d968371009c343f0647c8f27bb4ac2
thakyZ <950594+thakyZ@users.noreply.github.com>
2024-10-29 07:07:47 -0700

    update: Get-BackupDirectory works now (somewhat)

--- Get-BackupDirectory.ps1
+++ Get-BackupDirectory.ps1
@@ -3,27 +3,46 @@ using namespace System.IO;
 using namespace System.Management.Automation;
 using namespace System.ServiceProcess;
 
-[CmdletBinding(DefaultParameterSetName = "PartialPath")]
+[CmdletBinding(DefaultParameterSetName = 'PartialPath')]
 [OutputType([DirectoryInfo])]
 Param(
+  # TODO: Figure out why you only originally had a partial path parameter as you do not know.
   # Specifies a path to one or more locations.
   [Parameter(Mandatory = $True,
              Position = 0,
-             ParameterSetName = "PartialPath",
+             ParameterSetName = 'PartialPath',
              ValueFromPipeline = $True,
              ValueFromPipelineByPropertyName = $True,
-             HelpMessage = "A partial path to the backup location.")]
-  [Alias("PSPath","Path")]
+             HelpMessage = 'A partial path to the backup location.')]
+  [Alias('PSPath','Path')]
   [ValidateNotNullOrEmpty()]
   [string]
-  $PartialPath
+  $PartialPath,
+  # Specifies a path to one or more locations.
+  [Parameter(Mandatory = $True,
+             Position = 1,
+             ParameterSetName = 'PartialPath',
+             ValueFromPipeline = $True,
+             ValueFromPipelineByPropertyName = $True,
+             HelpMessage = 'The root path to the location the partial path is in.')]
+  [ValidateNotNullOrEmpty()]
+  [string]
+  $Root
 )
 
 Begin {
   [ActionPreference] $OriginalErrorActionPreference = $ErrorActionPreference;
+  $ErrorActionPreference = 'Continue';
   [DirectoryInfo] $Output;
 } Process {
-
+  [string] $BackupPath = (Join-Path -Path $Root -ChildPath "$($PartialPath).backup");
+  $Output = [DirectoryInfo]::new($BackupPath);
+  If (-not $Output.Exists) {
+    New-Item -Path $Output -ItemType Directory | Out-Null;
+  } Else {
+    Write-Error -ErrorId 'Get.BackupDirectory.AlreadyExists' -Exception [Exception]::new("Backup directory could not be created as it already exists at `"$($Output)`".") -Message "Backup directory could not be created as it already exists at `"$($Output)`"." | Out-Host;
+    $Output = $Null;
+  }
 } End {
   Write-Output -NoEnumerate -InputObject $Output;
 } Clean {
